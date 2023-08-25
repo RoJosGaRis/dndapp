5d8eaa51f1e3ada8b434290df567833b442fa3b2
Roberto García <roberto.jgr03@gmail.com>
2023-08-24 22:03:37 -0600

    Stylizing buttons

--- src/Components/Buttons/PageButtons.js
+++ src/Components/Buttons/PageButtons.js
@@ -12,38 +12,52 @@ const PageButtons = (props) => {
   return (
     <div className="page-buttons">
       <div className="page-buttons-wrapper">
-        <button
-          className={`page-buttons-button + ${
-            props.currentPage === 0 ? "active" : ""
-          }}`}
-          onClick={() => buttonPressHandler(0)}
-        >
-          <BiBookBookmark />
-        </button>
-        <button
-          className={`page-buttons-button + ${
-            props.currentPage === 1 ? "active" : ""
-          }}`}
-          onClick={() => buttonPressHandler(1)}
-        >
-          <IoCreate />
-        </button>
-        <button
-          className={`page-buttons-button + ${
-            props.currentPage === 2 ? "active" : ""
-          }}`}
-          onClick={() => buttonPressHandler(2)}
-        >
-          <BsFillChatLeftTextFill />
-        </button>
-        <button
-          className={`page-buttons-button + ${
-            props.currentPage === 3 ? "active" : ""
-          }}`}
-          onClick={() => buttonPressHandler(3)}
-        >
-          <GiRelationshipBounds />
-        </button>
+        <div className="page-buttons-button-wrapper">
+          <div className="page-buttons-button-cover"></div>
+          <button
+            className={`page-buttons-button ${
+              props.currentPage === 0 ? "active" : ""
+            }`}
+            onClick={() => buttonPressHandler(0)}
+          >
+            <BiBookBookmark />
+          </button>
+        </div>
+        <div className="page-buttons-button-wrapper">
+          <div className="page-buttons-button-cover"></div>
+          <button
+            className={`page-buttons-button ${
+              props.currentPage === 1 ? "active" : ""
+            }`}
+            onClick={() => buttonPressHandler(1)}
+          >
+            <IoCreate />
+          </button>
+        </div>
+        <div className="page-buttons-button-wrapper">
+          <div className="page-buttons-button-cover"></div>
+
+          <button
+            className={`page-buttons-button ${
+              props.currentPage === 2 ? "active" : ""
+            }`}
+            onClick={() => buttonPressHandler(2)}
+          >
+            <BsFillChatLeftTextFill />
+          </button>
+        </div>
+        <div className="page-buttons-button-wrapper">
+          <div className="page-buttons-button-cover"></div>
+
+          <button
+            className={`page-buttons-button ${
+              props.currentPage === 3 ? "active" : ""
+            }`}
+            onClick={() => buttonPressHandler(3)}
+          >
+            <GiRelationshipBounds />
+          </button>
+        </div>
       </div>
     </div>
   );
