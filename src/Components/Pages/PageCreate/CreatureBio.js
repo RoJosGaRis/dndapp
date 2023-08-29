1ee4471d5b92de431c9c83ce50349b684bab1d96
Roberto García <roberto.jgr03@gmail.com>
2023-08-28 22:46:02 -0600

    Added corrections to javascript condicionals

--- src/Components/Pages/PageCreate/CreatureBio.js
+++ src/Components/Pages/PageCreate/CreatureBio.js
@@ -4,19 +4,63 @@ const CreatureBio = (props) => {
   return (
     <div className={styles.creature_bio_wrapper}>
       <div className={styles.creature_bio_information_wrapper}>
-        <p className={styles.creature_bio_information}>{props.race}</p>
-        <p className={styles.creature_bio_information}>{props.alignment}</p>
-      </div>
-      <div className={styles.creature_bio_information_wrapper}>
-        <p className={styles.creature_bio_information}>HP: {props.hitPoints}</p>
-        <p className={styles.creature_bio_information}>
-          AC: {props.armorClass}
+        <p
+          className={
+            props.race === undefined
+              ? "loading"
+              : styles.creature_bio_information
+          }
+        >
+          {props.race === undefined ? "" : props.race}
         </p>
-        <p className={styles.creature_bio_information}>Speed: {props.speed}</p>
+        <p
+          className={
+            props.alignment === undefined
+              ? "loading"
+              : styles.creature_bio_information
+          }
+        >
+          {props.alignment === undefined ? "" : props.alignment}
+        </p>
       </div>
       <div className={styles.creature_bio_information_wrapper}>
-        <p className={styles.creature_bio_information}>
-          Challenge: {props.challenge}
+        <p
+          className={
+            props.hitpoints === undefined
+              ? "loading"
+              : styles.creature_bio_information
+          }
+        >
+          {props.hitpoints === undefined ? "" : `HP: ${props.hitPoints}`}
+        </p>
+        <p
+          className={
+            props.armorClass === undefined
+              ? "loading"
+              : styles.creature_bio_information
+          }
+        >
+          {props.armorClass === undefined ? "" : `AC: ${props.armorClass}`}
+        </p>
+        <p
+          className={
+            props.speed === undefined
+              ? "loading "
+              : styles.creature_bio_information
+          }
+        >
+          {props.speed === undefined ? "" : `Speed: ${props.speed}`}
+        </p>
+      </div>
+      <div className={styles.creature_bio_information_wrapper}>
+        <p
+          className={
+            props.challenge === undefined
+              ? "loading"
+              : styles.creature_bio_information
+          }
+        >
+          {props.challenge === undefined ? "" : `Challenge: ${props.challenge}`}
         </p>
       </div>
     </div>
--- src/Components/Pages/PageCreate/CreatureTraits.js
+++ src/Components/Pages/PageCreate/CreatureTraits.js
@@ -3,8 +3,10 @@ import styles from "./PageCreate.module.css";
 const CreatureTraits = (props) => {
   return (
     <div className={styles.creature_traits_wrapper}>
-      <p>Features and Traits</p>
-      <p>
+      <p className={styles.creature_traits_header}>{`${
+        props.featuresAndTraits === undefined ? "" : "Features and Traits"
+      }`}</p>
+      <p className={styles.creature_traits_body}>
         {props.featuresAndTraits === undefined
           ? " "
           : props.featuresAndTraits.split("$").map((feature) => {
