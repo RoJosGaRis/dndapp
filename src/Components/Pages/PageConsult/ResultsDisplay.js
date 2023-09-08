7c9321d8360573b9d71f82b432e8596386c2a736
RoJosGaRis <roberto.jgr03@gmail.com>
2023-09-07 22:38:53 -0600

    Added conditions schema

--- src/Components/Pages/PageConsult/ResultsDisplay.js
+++ src/Components/Pages/PageConsult/ResultsDisplay.js
@@ -1,6 +1,7 @@
 import AbilityScores from "../PageConsult/SchemasPages/AbilityScores";
 import Alignments from "../PageConsult/SchemasPages/Alignments";
 import Backgrounds from "./SchemasPages/Backgrounds";
+import Conditions from "./SchemasPages/Conditions";
 import styles from "./PageConsult.module.css";
 import PageConsult from "./PageConsult";
 
@@ -12,6 +13,7 @@ const ResultsDisplay = ({ results, type }) => {
       )}
       {type === "alignments" && <Alignments results={results}></Alignments>}
       {type === "backgrounds" && <Backgrounds results={results}></Backgrounds>}
+      {type === "conditions" && <Conditions results={results}></Conditions>}
     </div>
   );
 };
